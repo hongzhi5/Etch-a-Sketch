379bfa5ed697877b2d30241adcf4a87acd80f702
hongzhi5 <97701063+hongzhi5@users.noreply.github.com>
2023-08-04 18:09:37 -0500

    Add a feature when clicking a grid changes its color, and then hovering over other grids will also change their colors until clicking again to stop. Add a erase button to clean up.

--- javascript.js
+++ javascript.js
@@ -4,11 +4,11 @@ const slider = document.createElement("input");
 slider.type = "range";
 slider.min = 1;
 slider.max = 100;
-slider.value = 5; // default value set to the slider
+slider.value = 50; // default value set to the slider
 
 const sliderValue = document.createElement("div");
 sliderValue.classList.add("sliderValue");
-sliderValue.textContent = 5;
+sliderValue.textContent = 50;
 
 slider.addEventListener("input", (event) => {
     const tempSliderValue = event.target.value; 
@@ -20,9 +20,20 @@ const sliderContainer = document.querySelector("#sliderContainer");
 sliderContainer.appendChild(slider);
 sliderContainer.appendChild(sliderValue);
 
+
+// set two buttons for black and colorful mode
+const blackButton = document.querySelector("#black");
+const colorfulButton = document.querySelector("#colorful");
+var color = "black";
+blackButton.addEventListener("click", (event) => {
+    color = event.target.value;
+})
+colorfulButton.addEventListener("click", (event) => {
+    color = event.target.value;
+})
+
 // grid
 const gridContainer = document.querySelector('#container');
-
 function createGrid(numGrid) {
   const gridSize = 500 / numGrid; // Calculate the size of each grid cell
   container.innerHTML = ''; // Clear existing grids
@@ -32,20 +43,57 @@ function createGrid(numGrid) {
     gridItem.classList.add('gridItem');
     gridItem.style.width = gridSize + 'px';
     gridItem.style.height = gridSize + 'px';
-
     gridContainer.appendChild(gridItem);
+
+    //gridItem.addEventListener('mouseover', () => {
+        //gridItem.classList.add(`${color}`);
+      //});
+    // erase button
+    const eraseButton = document.querySelector("#erase");
+    eraseButton.addEventListener("click", (event) => {
+        gridItem.classList.remove("black");
+        gridItem.classList.remove("colorful");
+    })
   }
 
+// only start when click and stop when click again
+const gridItems = document.querySelectorAll('.gridItem');
+let colorChangingEnabled = false;
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener('click', () => {
+      colorChangingEnabled = !colorChangingEnabled;
+  
+      if (colorChangingEnabled) {
+        gridItem.classList.add(`${color}`);
+  
+        gridItems.forEach(item => {
+          item.addEventListener('mouseenter', () => {
+            if (colorChangingEnabled) {
+              item.classList.add(`${color}`);
+            }
+          });
+        });
+      } else {
+        gridItems.forEach(item => {
+          item.removeEventListener('mouseenter', null);
+          item.removeEventListener('mouseleave', null);
+        });
+      }
+    });
+  });
+
+// make sure the grids won't overflow and the container's size won't change
   const containerWidth = gridContainer.offsetWidth; // Get container width
   const itemSize = containerWidth / numGrid; // Calculate grid item size
   gridContainer.style.gridTemplateColumns = `repeat(${numGrid}, ${itemSize}px)`; // Set column template
   gridContainer.style.gridTemplateRows = `repeat(${numGrid}, ${itemSize}px)`; // Set row template
+
 }
 
 slider.addEventListener('input', function() {
   const numGrid = parseInt(slider.value);
   createGrid(numGrid);
 });
-
 // Initial grid creation with default slider value
 createGrid(parseInt(slider.value));
+
